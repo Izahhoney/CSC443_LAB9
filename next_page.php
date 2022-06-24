f542dbe323a38f1e417dfbf28fb3387cd2f43956
Izahhoney <2020489338@student.uitm.edu.my>
2022-06-24 13:56:29 +0800

    nextpage

--- next_page.php
+++ next_page.php
@@ -34,8 +34,8 @@ foreach ($_COOKIE as $name => $value)
     echo "$name = $value\n";
 }
 
-   $myLogin = $_SESSION["myLogin \n <br>"];
-   print ("Value of myLogin has been retrieved: ".$myLogin."\n </br>");
+   $myLogin = $_SESSION["myLogin"];
+   print ("\n<br> Value of myLogin has been retrieved: ".$myLogin."\n </br>");
    $myColor = $_SESSION["myColor"];
    echo "Value of myColor has been retrieved: ".$myColor."\n  </br>";
 
