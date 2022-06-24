aebe5b7eed3241e1263106ac50ccd2ed068da3ba
Izahhoney <2020489338@student.uitm.edu.my>
2022-06-24 13:18:44 +0800

    firstpage

--- first_page.php
+++ first_page.php
@@ -8,18 +8,19 @@
 </head>
 <body>
     <?php
-    session_start ();
-    echo"<html>";
-    $_SESSION ["mylogin"] = "myWebsite";
+    session_start();
+    echo "<html>";
+
+    $_SESSION["myLogin"] = "myWebsite";
     echo "A value saved in the session named as myLogin.<br>";
 
-    $_SESSION ["myColor"] = "Blue";
+    $_SESSION["myColor"] = "Blue";
     echo "A value saved in the session named as myColor.<br>";
 
-    echo "Click <a href=next_page.php>Next Page</a"
-        ." to retrieve the value.<br>";
+    echo "Click <a href=next_page.php>Next Page</a>"."to retrieve the values.<br";
+
     echo "</html><br>";
-    
     ?>
+    
 </body>
 </html>
