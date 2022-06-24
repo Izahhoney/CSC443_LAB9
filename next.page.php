129201b8a46fc67468bc5010406086decf19e4e3
Izahhoney <2020489338@student.uitm.edu.my>
2022-06-24 12:56:37 +0800

    nextpage

--- next.page.php
+++ next.page.php
@@ -11,10 +11,10 @@
     session_start ();
     echo"<html>";
 
-    $myLogin = $_SESSION["myLogin"];
+    $myLogin = $SESSION ["mylogin"];
     echo "Value of myLogin has been retrieved: ".$myLogin."\n";
 
-    $myColor = $_SESSION["myColor"];
+    $myColor = $SESSION ["myColor"];
     echo "Value of myColor has been retrieved: ".$myColor."\n";
 
     echo "</html>\n";
